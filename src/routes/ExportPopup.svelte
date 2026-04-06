0037e209f3439430d0c57ad88ea5b7895cfb2f63
Destructor-Ben <destructor.ben@gmail.com>
2026-04-07 01:47:15 +1200

    feat: blur on export popup

--- src/routes/ExportPopup.svelte
+++ src/routes/ExportPopup.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="absolute top-0 left-0 size-full bg-black/30 flex items-center justify-center" transition:fade={{ duration: 300 }}>
-  <div class="w-150 h-100 bg-ctp-base border-2 border-ctp-surface1 rounded-3xl p-6 shadow-2xl flex flex-col items-stretch justify-between" transition:fly={{ y: "100%", duration: 300 }}>
+  <div class="w-150 h-100 bg-ctp-base/90 backdrop-blur-lg border-2 border-ctp-surface1 rounded-3xl p-6 shadow-2xl flex flex-col items-stretch justify-between" transition:fly={{ y: "100%", duration: 300 }}>
     <h2 class="text-3xl text-center font-semibold">Export Fractal</h2>
 
     <p>TODO: implement exporting image + config</p>
